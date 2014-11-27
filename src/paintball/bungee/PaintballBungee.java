bcc93da178d5cd90969c6cc7c7d0eb8c75914d5d
xXJarJarXx <598Johnn897@gmail.com>
2014-11-27 18:01:15 -0500

    Update 11/27/14

---
+++ src/paintball/bungee/PaintballBungee.java
@@ -0,0 +1,31 @@
+/*
+ * Author: 598Johnn897
+ * 
+ * Date: Nov 27, 2014
+ * Package: paintball.bungee
+ *
+ */
+package paintball.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * 
+ */
+public class PaintballBungee extends Plugin
+{
+	@Override public void onLoad()
+	{
+		
+	}
+	
+	@Override public void onEnable()
+	{
+		
+	}
+	
+	@Override public void onDisable()
+	{
+		
+	}
+}
---
+++ src/paintball/bungee/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Author: 598Johnn897
+ * 
+ * Date: Nov 27, 2014
+ * Package: paintball.bungee
+ *
+ */
+/**
+ * 
+ */
+package paintball.bungee;
